4c74c71b079e317785e122658e309ac442378ebb
王冰 <wangbing.cs@bytedance.com>
2020-02-05 16:17:16 +0800

    feat: 第三部分over

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.template import loader
+from .models import Question, Choice
+
+# def my_render(request, filename, context):
+#   template = loader.get_template(filename)
+#   res = template.render(context, request)
+#   return HttpResponse(res)
 
 # Create your views here.
 def index(request):
-  return HttpResponse('index')
+  question_list = Question.objects.order_by('pub_date')
+  choice_list = Choice.objects.all()
+  res = ','.join([item.question_text for item in question_list])
+  res += '+'.join([item.choice_text for item in choice_list])
+  context = {
+    'question_list': question_list,
+  }
+  return render(request, 'index.html', context)
+
+
+def detail(request, question_id):
+  return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+  response = "You're looking at the results of question %s."
+  return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+  return HttpResponse("You're voting on question %s." % question_id)
