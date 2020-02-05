ca0fbe286c818b37735d917cf01f77981fb623d2
王冰 <wangbing.cs@bytedance.com>
2020-02-05 12:14:45 +0800

    save

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Question(models.Model):
   question_text = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
+  def __str__(self):
+    return self.question_text
 
 
 class Choice(models.Model):
